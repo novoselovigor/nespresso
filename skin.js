2757d06fd898e76dd3e47de6e571a4b59a0f9ae4
novoselovigor <62896286+novoselovigor@users.noreply.github.com>
2020-11-12 09:28:06 +0100

    756

--- skin.js
+++ skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 6.1.10/18007
 // Filename: silhouette_cardboard.ggsk
-// Generated 2020-11-12T07:25:03
+// Generated 2020-11-12T09:27:51
 
 function pano2vrSkin(player,base) {
 	player.addVariable('ht_ani', 2, false);
